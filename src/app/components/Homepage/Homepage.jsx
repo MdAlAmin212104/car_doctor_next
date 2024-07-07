23d60d3e166b009dfb9b39a3de2036bd07d3dce5
MD AL AMIN ISLAM <mdalamin212104@gmail.com>
2024-07-07 13:03:00 +0600

    banner section added

--- src/app/components/Homepage/Homepage.jsx
+++ src/app/components/Homepage/Homepage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Homepage = () => {
     return (
-        <div>
-            Home page
-        </div>
+        <Banner/>
     );
 };
 
